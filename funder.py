1defb601a626f718aafd9a794b6ade54746e6f94
wmr1 <wmr@cin.ufpe.br>
2018-04-12 22:25:02 -0300

    Funder message protocol, init and open channel messages

--- funder.py
+++ funder.py
@@ -1,4 +1,24 @@
-from messages import *
+import socket
+import messages, blockchain
+
+SOCK = None
 
 def execute():
-    print("Funder executed.")
+    blockchain.init()
+    print("Funder executed. Stabilising connection...", end='')
+    ip = input()
+    SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SOCK.connect((ip, 9735))
+
+    #send init message
+    SOCK.send(messages.init_message())
+
+    #receive init message
+    data = SOCK.recv(messages.MAX_LENGTH)
+    data = messages.parse_message(data)
+
+    #ask to open a channel
+    SOCK.send(messages.open_channel_message(
+        blockchain.HASH,
+        messages.generate_byte_array_string(8)
+    ))
