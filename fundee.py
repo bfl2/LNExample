62b61cc877735635e36f1dec3f68c6afbdd1b2f8
wmr1 <wmr@cin.ufpe.br>
2018-04-12 23:32:04 -0300

    Minor changes on fundee

--- fundee.py
+++ fundee.py
@@ -7,17 +7,18 @@ def sxor(s1,s2):
     return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
 
 def execute():
-    print("Fundee executed.")
+    print("Fundee executed, waiting for connection...", end='')
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind(('127.0.0.1', 9735))
     serversocket.listen(5)
+    (clientsocket, address) = serversocket.accept()
+    print(" Connected!")
 
     while(1):
-        (clientsocket, address) = serversocket.accept()
         # now do something with the clientsocket
         # in this case, we'll pretend this is a threaded server
         buffer = clientsocket.recv(65535)
-        messageDic = json.loads(buffer)
+        messageDic = json.loads(buffer.decode('utf-8'))
         type = ord(messageDic['type'])
         if (type == 16):
             clientsocket.send(init_message())
@@ -29,7 +30,7 @@ def execute():
             funding_output_index =messageDic['funding_output_index']
             channel_id = sxor(funding_txid, funding_output_index)
             signature = messageDic['signature']
-            clientsocket.sent(funding_signed_message(channel_id, signature))
+            clientsocket.send(funding_signed_message(channel_id, signature))
         elif(type == 36):
             channel_id = messageDic['channel_id']
             clientsocket.send(funding_locked_message(channel_id))
