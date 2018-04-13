bf421751373795fc94f938110335904955ddfe03
wmr1 <wmr@cin.ufpe.br>
2018-04-12 22:23:34 -0300

    Bug fix if no argument is passed

--- LNExample.py
+++ LNExample.py
@@ -2,10 +2,13 @@ import sys, funder, fundee
 
 def main(argv):
     print("Executing %s" %(argv[0]))
-    if(argv[1] == 'funder'):
-        funder.execute()
-    elif(argv[1] == 'fundee'):
-        fundee.execute()
+    if(len(argv) == 1):
+        print("You must pass an argument, funder or fundee. Exiting")
+    else:
+        if(argv[1] == 'funder'):
+            funder.execute()
+        elif(argv[1] == 'fundee'):
+            fundee.execute()
 
     return
 
