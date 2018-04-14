3c4ebaa1eff006ca3c3f3b1031d63f546838e0ba
wmr1 <wmr@cin.ufpe.br>
2018-04-14 17:16:58 -0300

    Completed fundee behaviour

--- fundee.py
+++ fundee.py
@@ -3,6 +3,7 @@ import socket
 import json
 import time
 
+SIGNATURE = generate_byte_array_string(64)
 
 def sxor(s1,s2):
     return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
@@ -23,7 +24,7 @@ def execute():
 
         except:
             print("buffer error")
-        messageDic = json.loads(buffer.decode('utf-8'))
+        messageDic = parse_message(buffer)
 
         type = ord(messageDic['type'])
         print("Type read:",type)
@@ -34,15 +35,24 @@ def execute():
             clientsocket.send(accept_channel_message(temporary_channel_id, 4))
         elif(type == 34):#received funding_created_message -> send funding_signed_message reply
             funding_txid = messageDic['funding_txid']
-            funding_output_index =messageDic['funding_output_index']
+            funding_output_index = messageDic['funding_output_index']
             channel_id = sxor(funding_txid, funding_output_index)
-            signature = generate_byte_array_string(64)
-            clientsocket.send(funding_signed_message(channel_id, signature))
+            clientsocket.send(funding_signed_message(channel_id, SIGNATURE))
         elif(type == 36):#received funding_locked_message -> send funding_locked_message reply
             #channel_id = messageDic['channel_id']
             clientsocket.send(funding_locked_message(channel_id))
             channel_closed_flag = 1
             print("funding locked!")
+        elif(type == 38):#received shutdown
+            clientsocket.send(shutdown_message(channel_id))
+        elif(type == 39):#close transaction
+            data = clientsocket.recv(MAX_LENGTH)
+            data = parse_message(data)
+            fee_received = data['fee_satoshis']
+            fair_fee = fee_received
+            clientsocket.send(closing_signed_message(channel_id, fair_fee, SIGNATURE))
+                
+
 
 
 
